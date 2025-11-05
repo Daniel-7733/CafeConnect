8efa66c7c080e81ed939ebe8ab05523b5d42f43d
Daniel Kazemian <achilles.7733@gmail.com>
2025-11-05 18:49:41 +0300

    change the format of list in html & add a box

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+from os import path, makedirs
 
 
 """
@@ -14,25 +14,40 @@ phone, or anything that charge with power)
 db: SQLAlchemy = SQLAlchemy()
 app: Flask = Flask(__name__)
 
+
+app.config["SECRET_KEY"] = "dev-secret"
+
+makedirs(app.instance_path, exist_ok=True)
+db_path: str = path.join(app.instance_path, "book.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
 class Cafe(db.Model):
     id: int = db.Column(db.Integer, primary_key=True)
-    name: str = db.Column(db.Column(db.String(100), nullable=False))
-    location: str = db.Column(db.Column(db.String(100), nullable=False))
-    wifi: str = db.Column(db.Column(db.String(100), nullable=False))
-    power: str =db.Column(db.Column(db.String(100), nullable=False))
-    link: str =db.Column(db.Column(db.String(1000), nullable=False))
+    name: str = db.Column(db.String(100), nullable=False)
+    location: str = db.Column(db.String(100), nullable=False)
+    wifi: str = db.Column(db.String(100), nullable=False)
+    power: str = db.Column(db.String(100), nullable=False)
+    link: str = db.Column(db.String(500), nullable=False)
 
 
 cafe: list[dict[str, str]] = [
-    {"Name": "Brewed Awakening", "Location": "E 1st Ave", "Wifi": "⚡ 📶Yes", "Power": "🔌Yes", "Link": "https://www.google.com/"},
-    {"Name": "Java Junction", "Location": "Clark Dr", "Wifi": "Yes", "Power": "No", "Link": "https://www.google.com/"}
+    {"Name": "Brewed Awakening", "Location": "E 1st Ave", "Wifi": "📶", "Power": "🔌", "Link": "https://www.brewedawakening.com/"},
+    {"Name": "cafe Medina", "Location": "Clark Dr", "Wifi": "📶", "Power": "🔌", "Link": "https://www.medinacafe.com/"}
     ]
-cafe_titles: list[str] = [key for element in cafe for key, value in element.items()]
+
+# cafe_titles: set[str] = set([key for element in cafe for key, value in element.items()])
 
 
 @app.route("/")
 def index() -> str:
-    return render_template("index.html", cafe_list=cafe, cafe_title=cafe_titles)
+    return render_template("index.html", cafe_list=cafe)
 
 # Todo: Add function to store new good Cafés or Cafés
 # Todo: Get information from the database
